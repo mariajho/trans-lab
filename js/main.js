681357b1edbee7fccd618b942afd8696c73122bd
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-07-14 10:40:16 -0400

    actualizando codigo en formulario con jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 var codigo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+$(document).ready(function(){
+	$("#boton").click(function(){
+		function email(email){
+			return codigo.test(email);
+		};
+		if($("#email").val() == true){
+			alert("tu email es valido");
+		}
+		if(email($("#email").val() == false){
+			e.preventDefault();
+			alert("tu email no es valido");
+		}
+		if($("contraseña").val().length != 8){
+			alert("la contraseña debe tener 8 digitos");
+		}else{
+			e.preventDefault();
+			window.location = "index-menu.html";
+
+		}
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+/*var codigo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 $(document).ready(function(){
 	$("#boton").click(function(){
 		var email = $("#email").val();
 		var contraseña = $("#contraseña").val();
-	};
+	});
 
 	if(email == ""){
 		$("#mensaje1").fadeIn();
@@ -16,4 +51,4 @@ $(document).ready(function(){
 		}
 	}
 
-});
+}*/
