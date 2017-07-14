15c54afb928806f5239034d336b3bb65e0153642
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-07-14 10:14:32 -0400

    intentando que funcione la validación del formulario

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-/* Holiii acá va tu código también */
+var codigo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+$(document).ready(function(){
+	$("#boton").click(function(){
+		var email = $("#email").val();
+		var contraseña = $("#contraseña").val();
+	};
+
+	if(email == ""){
+		$("#mensaje1").fadeIn();
+		return false;
+	}else{
+		$("#mensaje1").fadeOut();
+		if(contraseña == "" !expr.test(contraseña)){
+			$("#mensaje2").fadeIn();
+			return false;
+		}
+	}
+
+});
